09def71cf2b97fdaafebf2fab9e5a634451ce860
Victor Queiroga <victorqueiroga.jp@gmail.com>
2023-08-07 13:57:32 -0300

    docker files

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# Use the official Node.js image as the base image
+FROM node:18.17.0
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy package.json and yarn.lock to the container
+COPY package.json yarn.lock ./
+
+# Install Yarn globally (if it's not already installed in the Node.js image)
+#RUN npm install -g yarn
+
+# Install the application dependencies using Yarn
+RUN yarn install
+
+# Copy the rest of the application code to the container
+COPY . .
+
+# Build the Next.js application
+RUN yarn build
+
+# Expose the port on which Next.js is running (if using a custom port, change 3000 to your desired port)
+EXPOSE 3000
+
+# Start the Next.js application when the container starts
+CMD ["yarn", "dev"]
--- src/lgpd/Lgpd.tsx
+++ src/lgpd/Lgpd.tsx
@@ -13,7 +13,7 @@ const Lgpd = () => {
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const [cookiesAccepted, setCookiesAccepted] = useState(true);
     const [agreementDate, setAgreementDate] = useState(new Date());
-   
+
 
     useEffect(() => {
         // Perform localStorage action
@@ -25,10 +25,11 @@ const Lgpd = () => {
 
     }, [])
 
+    /*
     useEffect(() => {
         localStorage.setItem("agreementDate", format(agreementDate, 'yyyy-MM-dd') );
     },[agreementDate])
-
+*/
 
     const aceitar = () => {
         setCookiesAccepted(true);
@@ -61,10 +62,11 @@ const Lgpd = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                 <span>Utilizamos cookies e outras tecnologias pra melhorar a sua experiência no nosso site.</span>
                 <div>
-                    <button className="btn btn-sm" onClick={() => { 
+                    <button className="btn btn-sm" onClick={() => {
                         if (document) {
                             (document.getElementById('modal_policy') as HTMLFormElement).showModal();
-                        }}}>Regulamento</button>
+                        }
+                    }}>Regulamento</button>
                     <button className="btn btn-sm btn-primary" onClick={() => aceitar()}>Accept</button>
                 </div>
             </div>
